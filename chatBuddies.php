550e05eb41972b1c241484f7f071ad5a0318362b
Maryaen <mabou218@mtroyal.ca>
2024-10-06 03:18:19 -0600

    proper chat

--- chatBuddies.php
+++ chatBuddies.php
@@ -9,22 +9,10 @@
 
 <body>
 
-<ul>
-    
-    <li style= "float:left"><a href= "homepage.html"> Home</a> </li>
-    <li style= "float:left"><a class= "active" href= "findAbuddy.html"> Find Buddies</a></li>
-    <li style= "float:left"><a href= "chat.html"> Chat</a></li>
-    <li style= "float:right"><a href= "logout.html"> LogOut</a></li>
-    
-    </ul>
-</nav>
-
-
     <div class="container">
         <div class="user-list"> 
             <h3>Users</h3>
             <ul id="user-list">
-                <!-- Dynamically populated user list -->
                 <?php
                 $users = ['User1', 'User2', 'User3']; // Replace with actual user data retrieval
                 foreach ($users as $user) {
@@ -35,9 +23,7 @@
         </div>
 
         <div class="chat-container">
-            <!-- Private Chat Sections for Each User -->
             <div id="chat-sections">
-                <!-- Dynamically created chat sections -->
                 <?php
                 foreach ($users as $user) {
                     echo "<div id='chat-$user' class='private-chat' style='display: none;'>
